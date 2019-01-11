c96d17af73ad9a121fe0bc89904635e63e85994c
IsaakWeidman <44002541+IsaakWeidman@users.noreply.github.com>
2019-01-11 14:28:00 -0500

    takes input and splits into number and base

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class NumberConverter {
 
 	public static void main(String[] args) {
 
+	 	Scanner reader = new Scanner(System.in);
+	 	String input;
+	 	List<String> numbers;
+
+	 	System.out.print("Please enter the number and base formatted as \'number, base\'\n>");
+	 	input = reader.nextLine();
+
+		numbers = new ArrayList<>(input.split(","));
+	}
+
+
+
+	public static List<Integer> splitNumbers(String in) {
+
+		List<Integer> number;
 	}
 }
